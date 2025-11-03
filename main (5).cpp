a7228135cd464b940c8b43e7be03e44a1c2cc0a0
KEREM BULUT <fthmirmkrm@gmail.com>
2025-11-03 20:52:33 +0300

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    ofstream dosya2("dosya.txt");
+
+    for(int i = 0; i < 2; i++){
+        string adSoyad;
+        cin >> adSoyad;
+        dosya2 << adSoyad << endl;
+    }
+
+    dosya2.close();
+    return 0;
+}
